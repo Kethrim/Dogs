ec9db2e1276a5d0d7541c07e99551a404cdbf879
BrayanCaro <rojosuper@ciencias.unam.mx>
2019-11-28 14:18:55 -0600

    Solucione lo de las peticiones con la api Ahora el feed se llena con 2000 perros y hace un llamado automatico a la api Agregué las dependencias de (Gson para trabajar con json) al build.gradle El archivo RecyclerAdaptador fue borrado porque Keth tambien lo borraría Agregué al Manifest la opcion de pedir internet para hacer el llamado a las apis

---
+++ prueba/app/src/main/java/com/modelado/prueba/ApiCall.java
@@ -0,0 +1,232 @@
+package com.modelado.prueba;
+
+import android.os.AsyncTask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class ApiCall {
+
+    /**
+     * Clase ApiCall para ser un puente entre la api de la página web http://modelado2020-1.tk y una a-
+     * plicación para Android.
+     * <p>
+     * Esta api esta libre de excepciones, sin embargo es impresindible contar con conección a internet
+     * para mandar a llamar a todos y cada uno de los métodos.
+     *
+     * @author Brayan Martinez Santana Brayan
+     * @version 1.0
+     */
+    private Gson gson = new Gson(); // Sirve para pasar strings a objetos del tipo Json
+    private AppCompatActivity activity;
+
+    public ApiCall(AppCompatActivity activity) {
+        this.activity = activity;
+    }
+
+    /**
+     * Metodo que le agrega un comentario a un perro
+     * Acepta saltos de linea
+     *
+     * @param key        String que representa la key del usuario
+     * @param dog_id     String del perro al que se le hará el comentario
+     * @param comentario String Es el comentario mismo, funciona incluso con saltos de línea.
+     */
+    public void comentar(String key, String dog_id, String comentario) {
+        String url = "http://modelado2020-1.tk/comentarios.php?key=" + key + "&dog_id=" + dog_id;
+        try {
+
+            URL link = new URL(url);
+            String postData = "text=" + URLEncoder.encode(comentario, "UTF-8");
+            byte[] postDataBytes = postData.getBytes(StandardCharsets.UTF_8);
+
+            HttpURLConnection conn = (HttpURLConnection) link.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+            conn.getOutputStream().write(postDataBytes);
+            conn.getInputStream();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Obtiene todos los detalles de un perro en específico
+     * Cada índice representa cosas distintas
+     * > En el índice 0 representa el dog_id (este debe coincidir con el dog_id pasado como parámetro
+     * pero siempre es el mismo.
+     * > En el índice 1 representa el nombre del perro
+     * > En el índice 2 representa la imagen del perro en un url de internet
+     * > En el índice 3 representa el número de likes del perro
+     *
+     * @param key    String que representa la key del usuario
+     * @param dog_id String (único) del perro del que se obtendran los detalles
+     * @return String[] De  longitud 4 con los indices previamente mencionados
+     */
+    public String[] perroDetalles(String key, String dog_id) {
+        String url = "http://modelado2020-1.tk/detalles.php?key=" + key + "&dog_id=" + dog_id;
+
+        JsonObject jsonObj = gson.fromJson(apiLlamado(url), JsonObject.class);
+        JsonObject perro_info = jsonObj.get("perro").getAsJsonObject();
+        return new String[]{perro_info.get("dog_id").getAsString(),
+                perro_info.get("nombre").getAsString(),
+                perro_info.get("imagen").getAsString(),
+                perro_info.get("no_likes").getAsString(),
+        };
+    }
+
+    /**
+     * Obtiene todos los comentarios de un perro específico
+     * Es recomentable usar un ForEach para recorrer el arreglo que regresa porque si un usuario no
+     * tiene comentarios regresa un arreglo de longitud 0, o de cualquier otra longitud, dependiendo
+     * de la cantidad de comenatios del perrito.
+     *
+     * @param key    String que representa la key del usuario
+     * @param dog_id String (único) del perro del que se obtendran los detalles
+     * @return String[] De  longitud variada, donde cada elemento es un String que representa el
+     * comentario
+     */
+    public String[] perroComentarios(String key, String dog_id) {
+        String url = "http://modelado2020-1.tk/detalles.php?key=" + key + "&dog_id=" + dog_id;
+
+        JsonArray perro_coments = gson.fromJson(apiLlamado(url), JsonObject.class).get("comentarios").getAsJsonArray();
+        String[] comentarios = new String[perro_coments.size()];
+
+        for (int i = 0; i < comentarios.length; i++) {
+            comentarios[i] = perro_coments.get(i).getAsJsonObject().get("text").getAsString();
+        }
+        return comentarios;
+    }
+
+    /**
+     * Método que da 'Me gusta' a un perro en específico de parte del usuario con el que esta re-
+     * lacionada la key
+     *
+     * @param key    String que representa la key del usuario
+     * @param dog_id String (único) del perro al que se le dará like
+     */
+    public void likes(String key, String dog_id) {
+        String url = "http://modelado2020-1.tk/likes.php?key=" + key + "&dog_id=" + dog_id;
+        apiLlamado(url);
+    }
+
+    /**
+     * Obtiene la información de 2000 mil perros
+     * Donde
+     * > String[i][0] es un url de la imagene del i-ésimo perro
+     * > String[i][1] el nombre del i-ésimo perro
+     * > String[i][2] el número de likes del i-ésimo perro
+     * > String[i][3] el id del i-ésimo perro
+     *
+     * @param key La llave del usuario, de modo que solo los usuarios registrados pueden acceder a
+     *            este metodo
+     * @return String[][] de dimención 2000x4 donde el primer indice representa el i-ésimo perro, y
+     * cada perro tiene 4 atributos, mencionados arriba.
+     */
+    public String[][] feed(String key) {
+        String url = "http://modelado2020-1.tk/feed.php?key=" + key;
+        System.out.println("El resultado del llamado es" + apiLlamado(url));
+        JsonArray perros_json = this.gson.fromJson(apiLlamado(url), JsonArray.class);
+        String[][] dos_mil_perritos = new String[2000][4];
+
+        for (int i = 0; i < 2000; i++) {
+            JsonObject perro_json = perros_json.get(i).getAsJsonObject();
+            dos_mil_perritos[i][0] = perro_json.get("imagen").getAsString();
+            dos_mil_perritos[i][1] = perro_json.get("nombre").getAsString();
+            dos_mil_perritos[i][2] = perro_json.get("no_likes").getAsString();
+            dos_mil_perritos[i][3] = perro_json.get("perro_id").getAsString();
+        }
+
+        return dos_mil_perritos;
+    }
+
+    /**
+     * Sirve para hacer el login (iniciar sesión) de la api
+     *
+     * @param usr      El nombre del usario, es sensible a las mayusculas por lo que el usaurio
+     *                 "ABC" es distinto al usaurio "abc"
+     * @param password La contraseña del usuario
+     * @return String que representa la llave del usuario necesaria para mandar a llamar otros méto-
+     * dos
+     */
+    public String login(String usr, String password) {
+        String url = "http://modelado2020-1.tk/login.php?user=" + usr + "&pass=" + password;
+        JsonObject jsonObject = gson.fromJson(apiLlamado(url), JsonObject.class);
+        if (jsonObject.get("status").getAsString().equals("ok"))
+            return jsonObject.get("Message").getAsString(); // esto regresa el key
+        return "ERROR:usr or password incorrect";
+    }
+
+    /**
+     * Metodo para hacer el SingUp (Registro) en la api
+     *
+     * @param usr      El nombre del usuario, es sensible a las mayusculas por lo que el usaurio
+     *                 "ABC" es distinto al usaurio "abc"
+     * @param password La contraseña del usuario
+     * @return boolean true si usuario pudo registrase con éxito, false en otro caso
+     */
+    public boolean singUp(String usr, String password) {
+        String url = "http://modelado2020-1.tk/signup.php?user=" + usr + "&pass=" + password;
+        JsonObject jsonObject = gson.fromJson(apiLlamado(url), JsonObject.class);
+        return jsonObject.get("status").getAsString().equals("ok");
+    }
+
+    /**
+     * Sirve para hacer un llamado a la api
+     *
+     * @param url String con los parametros GET
+     * @return String que lo que haya sido responiddo por la pagina web.
+     */
+    private String apiLlamado(String url) {
+        MyTask a = new MyTask();
+        String salida = "";
+        try {
+            salida = a.execute(url).get();
+        } catch (Exception e) {
+            System.out.println("fallo MyTask" + e.getMessage());
+        }
+        return salida;
+    }
+
+    private class MyTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String url = strings[0];
+            StringBuffer response = new StringBuffer();
+            try {
+                URL urlForGetRequest = new URL(url);
+                String readLine = null;
+                HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+                conection.setRequestMethod("GET");
+                int responseCode = conection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(conection.getInputStream()));
+                    while ((readLine = in.readLine()) != null) {
+                        response.append(readLine);
+                    }
+                    in.close();
+                } else {
+                    System.out.println("Fallo detalles");
+                }
+            } catch (Exception e) {
+                System.out.println("DoInBack de MyTask error" + e.getMessage());
+                e.printStackTrace();
+            }
+            return response.toString();
+        }
+    }
+
+}
--- prueba/app/src/main/java/com/modelado/prueba/Feed.java
+++ prueba/app/src/main/java/com/modelado/prueba/Feed.java
@@ -15,7 +15,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.modelado.apicall.ApiCall;
+
 import com.squareup.picasso.Picasso;
 
 import java.io.BufferedInputStream;
@@ -28,7 +28,7 @@ import java.util.LinkedList;
 
 public class Feed extends AppCompatActivity {
 
-    private ApiCall api = new ApiCall();
+    private ApiCall api = new ApiCall(this);
     private RecyclerView mRv;
     private LinkedList<perro> perros;
 
@@ -59,11 +59,12 @@ public class Feed extends AppCompatActivity {
 
     public void llenaFeed(){
         perros = new LinkedList<>();
-        perros.add(new perro(R.drawable.uno,"Athan_Ervin",5, 76281));
-        perros.add(new perro(R.drawable.dos,"Alfonse_Barrett", 10, 47042));
-        perros.add(new perro(R.drawable.tres, "Zackery_Maverick", 6,41630));
-        perros.add(new perro(R.drawable.cuatro, "Max Wilbur", 79, 85653));
-        perros.add(new perro(R.drawable.cinco, "asdfghjklñqwertyuiopzxcvbnm", 95,36133));
+
+        String[][] dos_mil_perros = api.feed("961b6dd3ede3cb8ecbaacbd68de040cd78eb2ed5889130cceb4c49268ea4d506");
+        for (int i = 0; i < 2000; i++) {
+            perros.add(new perro(R.drawable.uno,dos_mil_perros[i][1],Integer.parseInt(dos_mil_perros[i][2]), Integer.parseInt(dos_mil_perros[i][3])));
+        }
+
     }
 
     /* El adaptador le pasa los datos al recycler view, pone las imágenes, etc */
