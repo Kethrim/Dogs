71b3865aa4019f7bae707b881c87bd292862fc6d
Kethrim <kethrim@ciencias.unam.mx>
2019-11-27 19:45:57 -0600

    Activity comentarios.

--- prueba/app/src/main/java/com/modelado/prueba/comentarios.java
+++ prueba/app/src/main/java/com/modelado/prueba/comentarios.java
@@ -1,17 +1,27 @@
 package com.modelado.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class comentarios extends AppCompatActivity {
 
+    private RecyclerView coments;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comentarios);
 
+        coments = findViewById(R.id.comentarios);
+        coments.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        LinearLayoutManager linearLayoutManager =  new LinearLayoutManager(this);
+        coments.setLayoutManager(linearLayoutManager);
+
+        
         Toast.makeText(this, "Preparando comentarios...", Toast.LENGTH_SHORT).show();
     }
 }
