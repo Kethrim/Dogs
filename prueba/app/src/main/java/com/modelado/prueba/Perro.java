50afc0cae625fd7ca1ecd7cc59b9b4a0e6e54f27
Kethrim <kethrim@ciencias.unam.mx>
2019-12-07 13:20:24 -0600

    Utilicé la api para descargar la imagen de cada perrito en el método correspondiente.

--- prueba/app/src/main/java/com/modelado/prueba/Perro.java
+++ prueba/app/src/main/java/com/modelado/prueba/Perro.java
@@ -92,57 +92,17 @@ public class Perro implements Serializable {
     }
 
     /**
-     * Descarga una imagen a partir de una url y es usada para el metodo onBindViewHolder con el fin
-     * de que cada imagen sea diferente
+     * Descarga una imagen a partir de la url del perro.
      * > El metodo puede fallar si no se cuenta con conexion a internet
      *
      * @return Un Bitmap que contiene toda la información de la imagen
      */
     public Bitmap descargaImg() {
-        PeticionAsynk a = new PeticionAsynk();
-        Bitmap salida = null;
-        try {
-            salida = a.execute(this.imagenPerro).get();
-        } catch (Exception e) {
-            System.out.println("Fallo al descargar la imagen: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-//        System.out.println("\t\t\t\tBITMAP DE LA IMAGEN"+salida);
-        return salida;
+        ApiCall api = new ApiCall();
+        Bitmap imagen = api.descargaImg(getImagenPerro());
+        return imagen;
     }
 
-    /**
-     * Clarse PeticionAsynk
-     * Esta clase representa una tarea de que realizará de forma asincrona en el programa
-     * Solo es usada una vez en descargImg
-     */
-    private class PeticionAsynk extends AsyncTask<String, Void, Bitmap> {
-
-        /**
-         * Este metodo se hace por detras y solo manda a descarar a la imagen por medio de una url y
-         * Obtiene un Bitmap de ella
-         *
-         * @param strings Url de la imagen empezando con el protocolo "https//"
-         * @return Bitmap de la imagen a la cual esta ligada la url en internet
-         */
-        @Override
-        protected Bitmap doInBackground(String... strings) {
-            String imageHttpAddress = strings[0];
-            Bitmap imagen = null;
-            URL imageUrl;
-
-            try {
-                imageUrl = new URL(imageHttpAddress);
-                HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
-                conn.connect();
-                imagen = BitmapFactory.decodeStream(conn.getInputStream());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-            return imagen;
-        }
-    }
 }
 
 
