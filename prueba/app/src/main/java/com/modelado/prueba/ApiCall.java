6acea3462b321a7b1bc69209128e7fac8a99ef8d
Kethrim <kethrim@ciencias.unam.mx>
2019-12-07 13:19:29 -0600

    Agregué el método de descargaImg() a éste archivo para que sea más fácil descargar la imagen.

--- prueba/app/src/main/java/com/modelado/prueba/ApiCall.java
+++ prueba/app/src/main/java/com/modelado/prueba/ApiCall.java
@@ -1,5 +1,7 @@
 package com.modelado.prueba;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -39,7 +42,7 @@ public class ApiCall {
      */
     public void comentar(String key, String dog_id, String comentario) {
         String url = "http://modelado2020-1.tk/comentarios.php?key=" + key + "&dog_id=" + dog_id;
-        System.out.println("ANTES DE LLAMAR A POST");
+
         apiLlamado(url, "POST", comentario);
     }
 
@@ -225,6 +228,61 @@ public class ApiCall {
                 }
             }
             return response.toString();
+        }
+    }
+
+
+    /**
+     * Descarga una imagen a partir de una url y es usada principalmente para el metodo onBindViewHolder con el fin
+     * de que cada imagen sea diferente
+     * > El metodo puede fallar si no se cuenta con conexion a internet
+     *
+     * @param url String De la url de la imagen, puede servir cualquier imagen en linea empezando por
+     *            "https://ruta_de_da_imagen"
+     * @return Un Bitmap que contiene toda la información de la imagen
+     */
+    public Bitmap descargaImg(String url) {
+        PeticionAsynkr a = new PeticionAsynkr();
+        Bitmap salida = null;
+        try {
+            salida = a.execute(url).get();
+        } catch (Exception e) {
+            System.out.println("Fallo al descargar la imagen: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return salida;
+    }
+
+    /**
+     * Clarse PeticionAsynk
+     * Esta clase representa una tarea de que realizará de forma asincrona en el programa
+     * Solo es usada una vez en descargImg
+     */
+    private class PeticionAsynkr extends AsyncTask<String, Void, Bitmap> {
+
+        /**
+         * Este metodo se hace por detras y solo manda a descarar a la imagen por medio de una url y
+         * Obtiene un Bitmap de ella
+         *
+         * @param strings Url de la imagen empezando con el protocolo "https//"
+         * @return Bitmap de la imagen a la cual esta ligada la url en internet
+         */
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String imageHttpAddress = strings[0];
+            Bitmap imagen = null;
+            URL imageUrl;
+
+            try {
+                imageUrl = new URL(imageHttpAddress);
+                HttpURLConnection conn = (HttpURLConnection) imageUrl.openConnection();
+                conn.connect();
+                imagen = BitmapFactory.decodeStream(conn.getInputStream());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            return imagen;
         }
     }
 
