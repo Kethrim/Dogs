d3e1cd80007eaa19a3b4b6a2923bb6d2cb25ad94
Kethrim <kethrim@ciencias.unam.mx>
2019-12-07 14:41:39 -0600

    Ya no crashea con los comentarios Agregué un toolbar, de manera que cada que regresas la actividad finaliza y el feed no se recarga. Modifiqué por lo tanto el layout y la clase de java, también tuve que modificar el manifiesto y el archivo styles para evitar una excepción.

--- prueba/app/src/main/java/com/modelado/prueba/ListaComentarios.java
+++ prueba/app/src/main/java/com/modelado/prueba/ListaComentarios.java
@@ -1,81 +1,36 @@
 package com.modelado.prueba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Bitmap;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.View;
 
-import org.w3c.dom.Text;
-
-import java.util.LinkedList;
 
 public class ListaComentarios extends AppCompatActivity {
-
-
-    TextView vistaDeListaDeComentariosDePerros,
-            postea,
-            nombrePerro,
-            idPerro,
-            meGustasPerro;
-
-    String idPerrrito;
-    String numMegustaPerrito;
-    String nombrePerrito;
-    LinkedList<String> listaDeComentariosDePerro;
-    EditText nuevoComentario;
-    String llaveUsuario;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_comentarios);
-        Bundle extras = getIntent().getExtras();
 
-        //Obtenemos los datos del perro a partir de lo que ya teníamos en el feed.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Comentarios");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-        TextView textView = findViewById(R.id.idPerroDetalles),
-                textView1 = findViewById(R.id.nombrePerroDetalles),
-                textView2 = findViewById(R.id.meGustaDetalles);
-
-        String id = textView.getText().toString(),
-                nombre = textView1.getText().toString(),
-                meGusta = textView2.getText().toString();
-
-        //Accedemos a la llave del usuario, que será necesaria para comentar
-        if (extras != null) {
-            llaveUsuario = extras.getString("llave");
-            idPerrrito = extras.getString("idPerrito");
-            nombrePerrito = extras.getString("nombrePerrito");
-            numMegustaPerrito = extras.getString("numMeGusta");
-        }
-
-
-
-        vistaDeListaDeComentariosDePerros = findViewById(R.id.listaComentarios);
-        nuevoComentario = findViewById(R.id.nuevoComentario);
-
-        postea = findViewById(R.id.postea);
-        nombrePerro = findViewById(R.id.nombrePerroDetalles);
-        idPerro = findViewById(R.id.idPerroDetalles);
-        meGustasPerro = findViewById(R.id.meGustaDetalles);
-
-        //Le damos formato al layout que usa ésta actividad
-        //falta la imagen
-        nombrePerro.setText("Nombre: " + nombrePerrito);
-        meGustasPerro.setText(numMegustaPerrito);
-        idPerro.setText("Id: " + idPerrrito);
-        vistaDeListaDeComentariosDePerros.setText(idPerrrito);
-
-
+        Intent intent = getIntent();
+        String llaveUsuario = intent.getStringExtra("llaveUsuario");
+        String idPerro = intent.getStringExtra("idPerro");
 
     }
 
-    private void obtenComentarios(int idPerro) {
-        ApiCall api = new ApiCall();
-//        api.perroComentarios();
-
-    }
 }
